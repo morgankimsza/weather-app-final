4d30d5bcdd16df0cdecdcfc4eef82ebff71ad5bb
morgankimsza <91759262+morgankimsza@users.noreply.github.com>
2021-11-18 10:23:07 +0100

    updated files
    updated files

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 function displayTemperature(response) {
   console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
 }
 
 let apiKey = "30c4c684c679265d7cee6b3521f0a4c2";
