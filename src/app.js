56b8880bdcb44ed0d7f61e08a72887515be65b4e
morgankimsza <91759262+morgankimsza@users.noreply.github.com>
2021-11-25 21:30:12 +0100

    api
    trying to intergrate api

--- src/app.js
+++ src/app.js
@@ -22,24 +22,26 @@ function formatDate(date) {
 
   return `${day} ${hours}:${minutes}`;
 }
-function displayForecast() {
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row" style="padding-left: 20px;">`;
-  let days = ["Thur", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
+
+  days.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `
             <div class="col-2">
-              <div class="weather-forecast-date">${day}</div>
+              <div class="weather-forecast-date">${forecastDay.dt}</div>
                 <img
-                  src="https://openweathermap.org/img/wn/10d@2x.png"
+                  src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
                   alt=""
                   width="42"
                 />
                 <div class="weather-forecast-temperature">
-                  <span class="weather-forecast-temperature-max"> 18° </span>
-                  <span class="weather-forecast-temperature-min"> 12° </span>
+                  <span class="weather-forecast-temperature-max">${forecastDay.temp.max} </span>
+                  <span class="weather-forecast-temperature-min">${forecastDay.temp.min}</span>
                 </div>
             </div>
       `;
